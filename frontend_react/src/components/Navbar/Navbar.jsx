344bb586d9a517e39a859b0c15b60c3822e41c9a
Jacob McMichael <jacobmcmichael@gmail.com>
2022-06-17 13:49:47 -0700

    Added navbar styles for desktop/mobile

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import './Navbar.scss';
 import { images } from '../../constants';
 
 const Navbar = () => {
+	const [toggle, setToggle] = useState(false);
+
 	return (
 		<nav className='app__navbar'>
 			<div className='app__navbar-logo'>
@@ -18,6 +22,33 @@ const Navbar = () => {
 					</li>
 				))}
 			</ul>
+
+			<div className='app__navbar-menu'>
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
+
+				{toggle && (
+					<motion.div
+						whileInView={{ x: [300, 0] }}
+						transition={{ duration: 0.85, ease: 'easeOut' }}
+					>
+						<HiX onClick={() => setToggle(false)} />
+						{['home', 'about', 'work', 'skills', 'contact'].map(
+							(item) => (
+								<ul>
+									<li key={item}>
+										<a
+											href={`#${item}`}
+											onClick={() => setToggle(false)}
+										>
+											{item}
+										</a>
+									</li>
+								</ul>
+							)
+						)}
+					</motion.div>
+				)}
+			</div>
 		</nav>
 	);
 };
